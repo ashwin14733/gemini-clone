a8d009f5c6843bc2375e62bfd9661e49d902865c
ashwin1473 <ashwindoss0304@gmail.com>
2025-12-16 01:06:08 +0530

    abc

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,30 +1,8 @@
-// import { GoogleGenerativeAI } from "@google/generative-ai";
-
-// // const ai = new GoogleGenerativeAI("AIzaSyDtD-KPcIRHrrCKRxwt1e_ONhzZv9-95JU");
-// const ai = new GoogleGenerativeAI("AIzaSyAKOAAnD5y4eNIMcsYSWCpNz-PGnJaucnI");
-
-
-// export const model = ai.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
-
-// async function main(prompt) {
-//   const result = await model.generateContent(prompt);
-//   console.log(result.response.text());
-
-
-//   let text = result.response.text();
-//   // remove the ** and * from the result
-//   // text = text.replace(/^\s*\*\s*(.*)$/gm, "<br><b>$1</b>");
-
-//   // text = text.replace(/\*/g,"");
-//   return text
-// }
-
-// export default main;
-
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const ai = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+// console.log(import.meta.env.VITE_GEMINI_API_KEY);
+
 
 export const model = ai.getGenerativeModel({
   model: "gemini-2.5-flash-lite",
